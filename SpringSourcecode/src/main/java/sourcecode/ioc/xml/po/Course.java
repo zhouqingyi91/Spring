4b906e6d30a41ddfeba122a1d4f66a28e0539492
zhouqingyi91 <luffy.zhouqingyi@icloud.com>
2019-09-20 22:17:25 +0800

    modify

--- SpringSourcecode/src/main/java/sourcecode/ioc/xml/po/Course.java
+++ SpringSourcecode/src/main/java/sourcecode/ioc/xml/po/Course.java
@@ -1,12 +1,20 @@
 package sourcecode.ioc.xml.po;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Course {
 
 	private String name;
 
+	public Course() {
+		System.out.println("course对象被创建");
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
--- SpringSourcecode/src/main/java/sourcecode/ioc/xml/po/Student.java
+++ SpringSourcecode/src/main/java/sourcecode/ioc/xml/po/Student.java
@@ -6,15 +6,21 @@ public class Student {
 
 	private Course course;
 
+	public Student() {
+		System.out.println("student对象被创建");
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
 
 	public void setCourse(Course course) {
 		this.course = course;
+		System.out.println("course对象被注入到student对象里");
 	}
 
 	public void initMethod() {
